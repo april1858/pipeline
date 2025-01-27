233ebc5ddbdb6e288ad858f622cdbcb83f1a8a4f
user <user@astra>
2025-01-27 15:59:15 +0700

    ok?

--- pipeline.go
+++ pipeline.go
@@ -14,13 +14,122 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Place your code here.
+	return finish(done, stg3(done, stg2(done, stg1(done, stg0(done, gen(done, in), stages[0]), stages[1]), stages[2]), stages[3]))
+}
+
+func gen(done In, in In) Out {
+	time.Sleep(2 * time.Millisecond) // !???
 	out := make(Bi)
 	go func() {
-		for i := 0; i < 6; i++ {
-			out <- in
-			time.Sleep(200 * time.Millisecond)
+		defer close(out)
+		for i := range in {
+			select {
+			case <-done:
+				return
+			case out <- i:
+			}
 		}
 	}()
+	return out
+}
 
+func stg0(done In, in In, stage Stage) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for i := range stage(in) {
+			select {
+			case <-done:
+				return
+			default:
+			}
+			select {
+			case <-done:
+				return
+			case out <- i:
+			}
+		}
+	}()
+	return out
+}
+
+func stg1(done In, in In, stage Stage) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for i := range stage(in) {
+			select {
+			case <-done:
+				return
+			default:
+			}
+			select {
+			case <-done:
+				return
+			case out <- i:
+			}
+		}
+	}()
+	return out
+}
+
+func stg2(done In, in In, stage Stage) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+
+		for i := range stage(in) {
+			select {
+			case <-done:
+				return
+			default:
+			}
+			select {
+			case <-done:
+				return
+			case out <- i:
+			}
+		}
+	}()
+	return out
+}
+
+func stg3(done In, in In, stage Stage) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for i := range stage(in) {
+			select {
+			case <-done:
+				return
+			default:
+			}
+			select {
+			case <-done:
+				return
+			case out <- i:
+			}
+		}
+	}()
+	return out
+}
+
+func finish(done In, in In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for i := range in {
+			select {
+			case <-done:
+				return
+			default:
+			}
+			select {
+			case <-done:
+				return
+			case out <- i:
+			}
+		}
+	}()
 	return out
 }
--- pipeline_test.go
+++ pipeline_test.go
@@ -2,6 +2,7 @@ package hw06pipelineexecution
 
 import (
 	"strconv"
+	"sync"
 	"testing"
 	"time"
 
@@ -12,6 +13,8 @@ const (
 	sleepPerStage = time.Millisecond * 100
 	fault         = sleepPerStage / 2
 )
+
+var isFullTesting = true
 
 func TestPipeline(t *testing.T) {
 	// Stage generator
@@ -35,31 +38,32 @@ func TestPipeline(t *testing.T) {
 		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
 		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
 	}
-	/*
-		t.Run("simple case", func(t *testing.T) {
-			in := make(Bi)
-			data := []int{1, 2, 3, 4, 5}
 
-			go func() {
-				for _, v := range data {
-					in <- v
-				}
-				close(in)
-			}()
+	t.Run("simple case", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
 
-			result := make([]string, 0, 10)
-			start := time.Now()
-			for s := range ExecutePipeline(in, nil, stages...) {
-				result = append(result, s.(string))
+		go func() {
+			for _, v := range data {
+				in <- v
 			}
-			elapsed := time.Since(start)
-			require.Equal(t, []string{"102", "104", "106", "108", "110"}, result)
-			require.Less(t,
-				int64(elapsed),
-				// ~0.8s for processing 5 values in 4 stages (100ms every) concurrently
-				int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
-		})
-	*/
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, nil, stages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Equal(t, []string{"102", "104", "106", "108", "110"}, result)
+		require.Less(t,
+			int64(elapsed),
+			// ~0.8s for processing 5 values in 4 stages (100ms every) concurrently
+			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
+	})
+
 	t.Run("done case", func(t *testing.T) {
 		in := make(Bi)
 		done := make(Bi)
@@ -89,4 +93,95 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+}
+
+func TestAllStageStop(t *testing.T) {
+	if !isFullTesting {
+		return
+	}
+	wg := sync.WaitGroup{}
+	// Stage generator
+	g := func(_ string, f func(v interface{}) interface{}) Stage {
+		return func(in In) Out {
+			out := make(Bi)
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				defer close(out)
+				for v := range in {
+					time.Sleep(sleepPerStage)
+					out <- f(v)
+				}
+			}()
+			return out
+		}
+	}
+
+	stages := []Stage{
+		g("Dummy", func(v interface{}) interface{} { return v }),
+		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
+		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
+		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
+	}
+
+	t.Run("done case", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		// Abort after 200ms
+		abortDur := sleepPerStage * 2
+		go func() {
+			<-time.After(abortDur)
+			close(done)
+		}()
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+		wg.Wait()
+
+		require.Len(t, result, 0)
+
+	})
+}
+
+func TestStg3(t *testing.T) { // test for "Stringifier"
+	stage := func(f func(v interface{}) interface{}) Stage {
+		return func(in In) Out {
+			out := make(Bi)
+			go func() {
+				defer close(out)
+				for v := range in {
+					time.Sleep(sleepPerStage)
+					out <- f(v)
+				}
+			}()
+			return out
+		}
+	}
+	stg := stage(func(v interface{}) interface{} { return strconv.Itoa(v.(int)) })
+	in := make(Bi)
+	data := []int{1, 2, 3, 4, 5}
+
+	go func() {
+		for _, v := range data {
+			in <- v
+		}
+		close(in)
+	}()
+
+	result := make([]string, 0, 10)
+	for s := range stg3(in, nil, stg) {
+		result = append(result, s.(string))
+	}
+	require.Equal(t, []string{"1", "2", "3", "4", "5"}, result)
 }
